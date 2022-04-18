efa994914ceb3fea6506857d75b47a0606e41189
Kat Rohrmeier <kat.rohrmeier@propellerhealth.com>
2022-04-17 19:45:20 -0500

    add DEBUG package & use instead of console.log

--- server.js
+++ server.js
@@ -2,15 +2,19 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const lookup = require('mime-types').lookup;
+const debug = require('debug')('debug');
 
 const PORT = 8080;
 
-// process.cwd is root of where the Node process starts; dirname is the name of directory of the file
-// change from where you call node server.js and these two will differ (bcs process.cwd will change)
-console.log('process.cwd = ', process.cwd());
-console.log('__dirname = ', __dirname);
+/* 
+  process.cwd is root of where the Node process starts; dirname is the name of directory of the file
+  change from where you call node server.js and these two will differ (bcs process.cwd will change)
+  uncomment the 2 console.logs below to see this in action :)
+*/
+debug('process.cwd = ', process.cwd());
+debug('__dirname = ', __dirname);
 
-// send in a dirname+filename fileToRead
+// send in a fileToRead in format of dirname+filename
 function promisifiedFileReading(fileToRead) {
   return new Promise((resolve, reject) => {
     fs.readFile(fileToRead, (error, fileData) => {
@@ -25,12 +29,12 @@ function promisifiedFileReading(fileToRead) {
 
 function createPathName(pathToJoin) {
   const requestedPath = path.join(__dirname, 'public', pathToJoin);
-  console.log('pathname created = ', requestedPath);
+  debug('pathname created = ', requestedPath);
   return path.join(requestedPath);
 }
 
 const server = http.createServer((req, res) => {
-  console.log('req.url = ', req.url);
+  debug('req.url = ', req.url);
 
   // remove leading and trailing slashes
   let resourceUrl = req.url.replace(/^\/+|\/+$/g, '');
@@ -44,19 +48,19 @@ const server = http.createServer((req, res) => {
 
   fs.readFile(filePath, (error, content) => {
     if (error) {
-      console.log('sending a 404');
+      debug('fs.readFile error. Sending a 404.');
       res.writeHead(404, { 'Content-Type': 'text/html' });
       return fs.createReadStream(createPathName('404.html')).pipe(res);
     } else {
       let mime = lookup(filePath);
-      console.log('returning %s of type %s', filePath, mime);
+      debug('returning %s of type %s', filePath, mime);
 
-      // if mime-type known, set header; otherwise, do not use Content-type header
+      // if mime-type known, then set header with it; otherwise, do not use Content-type header as per best practices
       if (mime) {
         res.writeHead(200, { 'Content-Type': mime });
         res.end(content);
       } else {
-        res.statusCode = 200;
+        res.writeHead(200);
       }
     }
   });
